88404402c0ea63fe3662b3f580c8547acc04495d
michal.sciubidlo <michal.sciubidlo@limango.pl>
2018-04-18 22:23:10 +0200

    basic construct for AI player, game still needs fixes - count points function

---
+++ computer_player.py
@@ -0,0 +1,96 @@
+
+from board import Board
+from random import randint
+
+
+class ComputerPlayer:
+
+    ALGORITHM_RANDOM = 0
+    ALGORITHM_GREEDY = 1
+    ALGORITHM_MINMAX = 2
+    ALGORITHM_ALPHABETA = 3
+
+    def __init__(self, alg_type):
+        """
+        Initialize artificial player with chosen algorithm to play
+        :param alg_type: algorithm type that decides ComputerPlayer moves
+        """
+        if alg_type not in [ComputerPlayer.ALGORITHM_RANDOM, ComputerPlayer.ALGORITHM_GREEDY, ComputerPlayer.ALGORITHM_MINMAX, ComputerPlayer.ALGORITHM_ALPHABETA]:
+            print("No matched algorithm for input " + str(alg_type) + ". Choosing ALGORITHM_RANDOM. ")
+            self.alg_type = ComputerPlayer.ALGORITHM_RANDOM
+        else:
+            self.alg_type = alg_type
+
+    def get_move(self, board: Board):
+        """
+        Returns (x, y) coords of move chosen by computer according to chosen algorithm for board passed as a parameter
+        Returns only coords that are free at moment of choosing - can be taken
+        :param board: game board
+        :return: tuple (x: int, y: int)
+        """
+        if self.alg_type == ComputerPlayer.ALGORITHM_RANDOM:
+            return self.random_move(board)
+        elif self.alg_type == ComputerPlayer.ALGORITHM_GREEDY:
+            return self.greedy_move(board)
+        elif self.alg_type == ComputerPlayer.ALGORITHM_ALPHABETA:
+            return self.alphabeta_move(board)
+        else:
+            return self.minmax_move(board)
+
+    def random_move(self, board: Board):
+        """
+        Returns random (x, y)
+        :param board:
+        :return:
+        """
+        x, y = (randint(0, board.size - 1), randint(0, board.size - 1))
+        while board.data[x][y] != 0:
+            x, y = (randint(0, board.size - 1), randint(0, board.size - 1))
+        return x, y
+
+    def greedy_move(self, board: Board):
+        """
+        Returns (x, y) coords that give the most benefit on short term.
+        :param board:
+        :return:
+        """
+        best_x = 0
+        best_y = 0
+        best_gain = board.count_points(best_x, best_y)
+        for y in range(board.size):
+            for x in range(board.size):
+                if board.data[x][y] != 0:
+                    continue  # don't check position already taken
+                gain = board.count_points(x, y)
+                print("gain(" + str(x) + "," + str(y) + ") gain: " + str(gain))
+                if gain > best_gain:
+                    best_gain = gain
+                    best_x = x
+                    best_y = y
+        return best_x, best_y
+        pass
+
+    def minmax_move(self, board: Board):
+        """
+        Returns (x,y) move chosen by minmax algorithm
+        :param board:
+        :return:
+        """
+        pass
+
+    def alphabeta_move(self, board: Board):
+        """
+        Returns (x,y) move chosen by alpha beta pruning algorithm
+        :param board:
+        :return:
+        """
+        pass
+
+    def get_gain(self, board, x, y):
+        """
+        Return possible gain points
+        :param board:
+        :param x:
+        :param y:
+        :return:
+        """
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from board import Board
 from random import randint
+from computer_player import ComputerPlayer
 
 
 def get_computer_input(board):
@@ -20,34 +21,28 @@ counter = 0
 
 computer_points = 0
 player_points = 0
+cp = ComputerPlayer(ComputerPlayer.ALGORITHM_GREEDY)
 
 while loop:
-    # if counter % 2 != 0:
-    #     (x, y) = get_computer_input(board)
-    #     while board.data[x][y] != 0:
-    #         (x, y) = get_computer_input(board)
-    #     points = board.insert_pos(x, y)
-    #     computer_points += points
-    #     print("computer gets: " + str(points))
-    # else:
-    #     inp = input(menu)
-    #     if inp == "quit" or inp == "exit" or inp == "q":
-    #         loop = False
-    #     (x, y) = (inp.split(" ")[0], inp.split(" ")[1])
-    #     points = board.insert_pos(int(x), int(y))
-    #     print("player gets: " + str(points))
-    #     player_points += points
-    inp = input(menu)
-    if inp == "quit" or inp == "exit" or inp == "q":
-        loop = False
-
-    (x, y) = (inp.split(" ")[0], inp.split(" ")[1])
-    points = board.insert_pos(int(x), int(y))
-    print("player gets: " + str(points))
-    player_points += points
-
+    if counter % 2 != 0:
+        (x, y) = cp.get_move(board)
+        print("computer chooses: (" + str(x) + "," + str(y) + ")")
+        points = board.insert_pos(x, y)
+        computer_points += points
+        print("computer gets: " + str(points))
+    else:
+        inp = input(menu)
+        (x, y) = (inp.split(" ")[0], inp.split(" ")[1])
+        points = board.insert_pos(int(x), int(y))
+        print("player gets: " + str(points))
+        player_points += points
     counter += 1
     print(board)
     print("P: " + str(player_points))
     print("C: " + str(computer_points))
+    # (x, y) = (inp.split(" ")[0], inp.split(" ")[1])
+    # points = board.insert_pos(int(x), int(y))
+    # print("player gets: " + str(points))
+    # player_points += points
+
 
