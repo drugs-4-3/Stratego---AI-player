f61e4879780cd368c678e77394e4fe4a09393759
michal.sciubidlo <michal.sciubidlo@limango.pl>
2018-05-18 12:08:55 +0200

    added some tests

--- tests.py
+++ tests.py
@@ -4,20 +4,20 @@ from board import Board
 
 class TestStrategoMethods(unittest.TestCase):
     """
-    Tests depend on test_board.txt file, that contain such board:
+    Tests depend on test_board.txt file, that contains such board:
 
     0 1 1 1 1
     1 1 0 0 1
     1 1 1 1 1
     1 1 1 1 1
     1 1 1 1 1
-
     """
 
     def test_loading_from_file(self):
         board = Board.load_from_file("test_board.txt")
         self.assertTrue(board.data[0][0] == 0)
-        self.assertTrue(board.data[0][1] == 1)
+        self.assertTrue(board.data[3][1] == 1)
+
 
     def test_board_copy(self):
         """
@@ -36,17 +36,17 @@ class TestStrategoMethods(unittest.TestCase):
 
     def test_available_positions(self):
         """
-        Make sure that available positions are being properly delivered
+        Make sure that all available positions are being properly returned
         :return:
         """
         board = Board.load_from_file("test_board.txt")
         available_positions = board.get_available_positions()
         self.assertEqual(3, len(available_positions))
         self.assertTrue((0, 0) in available_positions)
-        self.assertTrue((2, 1) in available_positions)
+        self.assertTrue((1, 2) in available_positions)
 
     def test_gaining_points(self):
-        # TODO: implement method for filling crossed lines for board loaded from file 
+        # TODO: implement method for filling crossed lines for board loaded from file
         pass
 
     def test_choosing_selected_position(self):
